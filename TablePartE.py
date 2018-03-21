b014c957427ee94343a88ce3d694a28522f98027
Chris Han <chris.han8@gmail.com>
2018-03-20 21:13:05 -0400

    finished build of partE

--- TablePartE.py
+++ TablePartE.py
@@ -1,2 +1,10 @@
 import happybase as hb
 
+connection = hb.Connection()
+
+table = connection.table('powers')
+columns = (b'personal:hero', b'personal:power', b'professional:name', b'professional:xp', b'custom:color')
+for key, data in table.scan(columns=columns, include_timestamp=True):
+    print('Found: {}, {}'.format(key, data))
+
+connection.close()
