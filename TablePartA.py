a622ea7dd773cff7b4c885808db568ebee1e68a4
Chris Han <chris.han8@gmail.com>
2018-03-20 20:08:01 -0400

    finished developing part A

--- TablePartA.py
+++ TablePartA.py
@@ -1,1 +1,13 @@
 import happybase as hb
+
+connection = hb.Connection('hostname')
+powersTable = connection.create_table('powers', {
+    'personal': dict(),
+    'professional': dict(),
+    'custom': dict()
+})
+
+foodsTable = connection.create_table('food', {
+    'nutrition': dict(),
+    'taste': dict()
+})
