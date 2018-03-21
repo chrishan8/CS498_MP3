d57485bff1a2ebe58fe402ea06a68234b4d8fd03
Chris Han <chris.han8@gmail.com>
2018-03-20 20:25:37 -0400

    fixing output of partB

--- TablePartB.py
+++ TablePartB.py
@@ -1,10 +1,11 @@
 import happybase as hb
+import re
 
 connection = hb.Connection()
 
 print(connection.tables())
 
 for table in connection.tables():
-    print(table.split("\'")[1])
+    print(re.sub("[\',b]", '', table))
 
 connection.close()
