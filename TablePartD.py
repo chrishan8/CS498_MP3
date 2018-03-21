30699db34218fe9e2e76b6b4f770a3e51414fd29
Chris Han <chris.han8@gmail.com>
2018-03-20 21:09:37 -0400

    finished build of partD

--- TablePartD.py
+++ TablePartD.py
@@ -1,2 +1,11 @@
 import happybase as hb
 
+connection = hb.Connection()
+
+table = connection.table('powers')
+row1 = table.row(b'row1')
+row19 = table.row(b'row19')
+
+print('hero: {}, power: {}, name: {}, xp: {}, color: {}'.format(row1[b'personal:hero'], row1[b'personal:power'], row1[b'professional:name'], row1[b'professional:xp'], row1[b'custom:color']))
+print('hero: {}, color: {}'.format(row19[b'personal:hero'], row19[b'custom:color']))
+print('hero: {}, name: {}, color: {}'.format(row1[b'personal:hero'], row1[b'professional:name'], row1[b'custom:color']))
