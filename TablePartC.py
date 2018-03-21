24bb066c855e19f90464ae05b0d0a40f55896415
Chris Han <chris.han8@gmail.com>
2018-03-20 20:56:59 -0400

    fixing partC

--- TablePartC.py
+++ TablePartC.py
@@ -7,8 +7,7 @@ table = connection.table('powers')
 with open('input.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        values = row.split[',']
-        [rowId, hero, power, name, xp, color] = values.map(lambda x: x.encode())
+        [rowId, hero, power, name, xp, color] = row.map(lambda x: x.encode())
         data = {
             b'personal:hero': hero,
             b'personal:power': power,
@@ -18,4 +17,4 @@ with open('input.csv', newline='') as csvfile:
         }
         table.put(rowId, data)
 
-connection.close()   
+connection.close()
