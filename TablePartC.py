c8c9227e1aef4ae378d469585c76ad63a2d7a71c
Chris Han <chris.han8@gmail.com>
2018-03-20 20:53:48 -0400

    finished build for partC

--- TablePartC.py
+++ TablePartC.py
@@ -1,3 +1,21 @@
 import happybase as hb
 import csv
 
+connection = hb.Connection()
+
+table = connection.table('powers')
+with open('input.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        values = row.split[',']
+        [rowId, hero, power, name, xp, color] = values.map(lambda x: x.encode())
+        data = {
+            b'personal:hero': hero,
+            b'personal:power': power,
+            b'professional:name': name,
+            b'professional:xp': xp,
+            b'custom:color': color
+        }
+        table.put(rowId, data)
+
+connection.close()   
